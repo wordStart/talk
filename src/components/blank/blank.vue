cc5378caf8fdabf2f97bc15cd36bbc622f101b65
haozewen <word_start@163.com>
2017-11-13 19:00:56 +0800

    talk

---
+++ src/components/blank/blank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="blank"></div>
+</template>
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .blank{
+    width: 100%;
+    height: 10px;
+    background: #fff;
+  }
+</style>
---
+++ src/container/login/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Field label="用户" placeholder="请输入用户名" v-model="username"></Field>
+    <Blank />
+    <Field label="密码" placeholder="请输入密码" v-model="password"></Field>
+    <Blank />
+    <Button type="primary" style="width:100%">登录</Button>
+    <Blank />
+    <Button type="primary" style="width:100%" @click="register">注册</Button>
+  </div>
+</template>
+<script>
+import { Field, Button } from 'mint-ui'
+import Blank from '../../components/blank/blank'
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  components: {
+    Field,
+    Button,
+    Blank
+  },
+  methods: {
+    register() {
+      console.log(1)
+    }
+  }
+}
+</script>
